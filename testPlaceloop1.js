ad48bd2bfa434dd2b62724a2bfdadf0e5e3c60f2
Camille <camille.sayous@epitech.eu>
2022-04-28 02:01:30 +0200

    [ADD] check collision and [FIX] sort hours

--- testPlaceloop1.js
+++ testPlaceloop1.js
@@ -1,30 +1,58 @@
 let { daysSelected, hoursToAdd, openingHours } = require('./testPlaceloopData.js');
+let checkDigit = false;
 
 const addSchedule = (daysSelected, hoursToAdd) => {
-    for (let day in daysSelected) {
-        if (daysSelected.hasOwnProperty(day) && daysSelected[day]) {
-            openingHours[day].push(hoursToAdd);
-            openingHours[day] = sortOpeningHoursAscending(openingHours[day]);
+    Object.keys(openingHours).forEach(day => {
+        let timeFree = checkCollisions(day);
+        if (daysSelected[day] && !checkDigit && timeFree) {
+                openingHours[day].push(hoursToAdd);
+                sortOpeningHoursAscending(openingHours[day]);
+        } else if (!timeFree) {
+            stopTest("L'horaire n'est pas disponible");
         }
-    }
+    });
+}
+
+const checkCollisions = (day) => {
+    let timeFree = true;
+    openingHours[day].forEach(hours => {
+        hoursBegin = getHoursAndMinutesDays(hours.begin);
+        hoursEnd = getHoursAndMinutesDays(hours.end);
+        hoursToAddBegin = getHoursAndMinutesDays(hoursToAdd.begin);
+        hoursToAddEnd = getHoursAndMinutesDays(hoursToAdd.end);
+
+        if ((hoursToAddBegin > hoursBegin && hoursToAddBegin < hoursEnd) || (hoursToAddBegin < hoursBegin && hoursToAddEnd > hoursBegin) ||
+            (hoursToAddBegin < hoursEnd && hoursToAddEnd >= hoursEnd) || (hoursToAddBegin >= hoursBegin && hoursToAddEnd <= hoursEnd) ||
+            (hoursToAddBegin <= hoursBegin && hoursToAddEnd >= hoursEnd)) {
+                timeFree = false;
+        }
+    });
+    return timeFree;
 };
 
 const getHoursAndMinutesDays = (hours) => {
-    const hoursDays = hours.split(":");
-    const getHoursDays = parseInt(hoursDays[0]);
-    const getMinutesDays = parseInt(hoursDays[1]);
+    const hoursDays = hours.split(':');
+    const getHoursDays = hoursDays[0];
+    const getMinutesDays = hoursDays[1];
+    const hoursConverted = getHoursDays + getMinutesDays / 100;
 
-    console.log(getHoursDays, getMinutesDays);
-    
-    return { getHoursDays, getMinutesDays };
+    if (getHoursDays < 0 || getMinutesDays < 0 || isNaN(hoursConverted)) {
+        checkDigit = true;
+        stopTest("L'horaire est invalide");
+    }    
+    return hoursConverted;
 };
 
 const sortOpeningHoursAscending = (hours) => {
-    return hours.sort((a, b) => {
-        const { getHoursDays: hourDayA, getMinutesDays: minuteDayA } = getHoursAndMinutesDays(a.begin);
-        const { getHoursDays: hourDayB, getMinutesDays: minuteDayB } = getHoursAndMinutesDays(b.begin);
-        return ((hourDayA - hourDayB) || ((hourDayA === hourDayB) && (minuteDayA - minuteDayB)));
+    hours.sort((a, b) => {
+        const hourA = getHoursAndMinutesDays(a.begin);
+        const hourB = getHoursAndMinutesDays(b.begin);
+        return hourA - hourB;
     });
+};
+
+const stopTest = (reason) => {
+    throw new Error(reason);
 };
 
 addSchedule(daysSelected, hoursToAdd);
